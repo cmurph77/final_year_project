3d54c797bea44069669472d1f69928286808a641
cmurph77 <91293683+cmurph77@users.noreply.github.com>
2024-01-12 13:38:27 +0000

    foudn shortest path and routed a vehicle along it

--- Network1/traci-scripts/v1.py
+++ Network1/traci-scripts/v1.py
@@ -1,5 +1,4 @@
-# Goal: Find the shortest path in network and then send a vehicle along that route.
-
+# this scripts creates a simulation with a single trip from the start edge to the end edge
 import sys
 import traci
 
@@ -10,7 +9,7 @@ def add_vehicle(route, depart,type,id):
     return vehicle
 
 def create_roufile():
-# HEADER
+    # header
     xml_content = """<?xml version="1.0" encoding="UTF-8"?>
 <!-- generated on 2023-11-15 14:45:18 by Eclipse SUMO netedit Version 1.18.0
 -->
@@ -18,27 +17,27 @@ def create_roufile():
 <routes xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/routes_file.xsd">
     <!-- Routes -->
         """
-# Routes
-    # xml_content = xml_content + """ <route id="br_tl" edges="start_e  -E3 E10" color="cyan"/>"""
+    # Routes
+    # xml_content = xml_content + """
+    #     <route id="br_tl" edges="-E11 E8 -E3 E10" color="cyan"/>
+    #     <route id="tr_bl" edges="E0 -E4 -E5 -E6" color="cyan"/> 
+    #     <route id="tr_bl" edges="E0 -E4 -E5 -E6" color="cyan"/>"""
+    xml_content = xml_content + """<trip id="t_0" depart="0.00" from="start_e" to="end_e"/>"""
 
 
-# Trips
-    # xml_content = xml_content + """<trip id="t_0" depart="0.00" from="start_e" to="end_e"/>"""
-
-
-
-# Vehicles
     xml_content = xml_content + """
-    <vType id="car" accel="0.8" decel="4.5" sigma="0.5" length="5" minGap="2.5" maxSpeed="10" guiShape="passenger"/>"""
+    <vType id="typeWE" accel="0.8" decel="4.5" sigma="0.5" length="5" minGap="2.5" maxSpeed="10" guiShape="passenger"/>"""
+    
+    # vehicles
 
-    # for i in range (1,6):  
+    # for i in range (1,6):
     #     xml_content = xml_content + add_vehicle('br_tl',0,'typeWE',i)
     
     # for i in range (6,11):
     #     xml_content = xml_content + add_vehicle('tr_bl',0,'typeWE',i)
     
     
-# Footer
+    # footer
     xml_content = xml_content + """
 </routes>"""
 
@@ -46,21 +45,20 @@ def create_roufile():
     with open("net1.rou.xml", "w") as file:
         file.write(xml_content)
 
- 
+def find_shortestpath():
+    startEdge = 'start_e'
+    endEdge = 'end_e'
+    routeInfo = traci.simulation.findRoute(startEdge, endEdge)
+
+    print(routeInfo)   
 
 def run():
     # Simulation loop
     step = 0
-    count = traci.vehicle.getIDCount()
-    # print('vehicle count', count)
-    routeInfo = traci.simulation.findRoute('start_e', 'end_e')
-    print(routeInfo.edges)
-    traci.route.add('route1', routeInfo.edges)
-    traci.vehicle.add(1,typeID='car', routeID='route1')
+    traci.vehicle.getIDCount()
 
-    while step < 1000:
+    while step < 50:
         traci.simulationStep()
-
         # Your simulation logic here
         step += 1
 
